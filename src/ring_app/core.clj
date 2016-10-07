82295c21273c1ee2c48f26533069ba5d1cd2a0bf
morrxy <morrxy@gmail.com>
2016-10-07 16:25:47 +0800

    first commit

---
+++ src/ring_app/core.clj
@@ -0,0 +1,36 @@
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :as compojure]
+            [ring.util.http-response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.format :refer [wrap-restful-format]]))
+
+(defn response-handler [request]
+  (response/ok
+    (str "<html><body> your IP is: "
+         (:remote-addr request)
+         "</body></html>")))
+
+(compojure/defroutes handler
+                     (compojure/GET "/" request response-handler)
+                     (compojure/GET "/foo" request (interpose ", " (keys request)))
+                     (compojure/GET "/:id" [id] (str "<p>the id is " id "</p>"))
+                     (compojure/POST "/json" [id] (response/ok {:result id})))
+
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
+(defn wrap-formats [handler]
+  (wrap-restful-format
+    handler
+    {:formats [:json-kw :transit-json :transit-msgpack]}))
+
+(defn -main []
+  (jetty/run-jetty
+    (-> #'handler wrap-nocache wrap-reload wrap-formats)
+    {:port 3000
+     :join? false}))
+
